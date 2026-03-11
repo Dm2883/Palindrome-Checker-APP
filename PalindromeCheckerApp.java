d88063512d7ec390173dbcb57952aec0a599c5f5
Dm2883 <devarajmurugan45@gmail.com>
2026-03-11 22:25:43 +0530

    UC2: Hardcoded palindrome logic

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,20 +1,27 @@
-public class PalindromeCheckerApp{
+import java.util.Scanner;
 
-    private static final String VERSION = "1.0";
-
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        displayWelcomeMessage();
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
 
-    private static void displayWelcomeMessage() {
+        boolean isPalindrome = true;
 
-        System.out.println("==============================================");
-        System.out.println("     Welcome to Palindrome Checker System     ");
-        System.out.println("==============================================");
-        System.out.println("Version : " + VERSION);
-        System.out.println("System initialized successfully.");
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
     }
 }
